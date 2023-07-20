2ebde826997fc1bd351171b9321804d623136334
Bryan H <bryanhui77@hotmail.com>
2023-07-19 17:37:46 -0700

    first jQuery function works

---
+++ script.js
@@ -0,0 +1,6 @@
+function f() {
+    console.log(document.getElementById("trial-button").textContent);
+
+    $("<img class='gif' src='pictures/Volleyball Game Demo.gif' alt='gif'>").appendTo("#content-panel");
+
+}
